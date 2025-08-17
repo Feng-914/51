ace868da6c4e1044ea1795d5ed3461948f48bb93
Feng-914 <fengerxu0914@outlook.com>
2025-08-17 14:50:21 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,67 @@
+//#include <REGX52.H>
+//#include <INTRINS.H>
+//void Delay1ms(unsigned int xms)		//@12.000MHz
+//{
+//	unsigned char i, j;
+//	while (xms)
+//	{
+//		i = 12;
+//		j = 169;
+//		do
+//		{
+//			while (--j);
+//		} while (--i);
+//		xms--;
+//	}
+
+//}
+//
+//void main()
+//{
+//	P2 = 0xFE;	//1111 1110
+//	Delay1ms(500);
+//	P2 = 0xFD;  //1111 1101
+//	Delay1ms(500);
+//	P2 = 0xFB;	//1111 1011
+//	Delay1ms(500);
+//	P2 = 0xF7;	//1111 0111
+//	Delay1ms(500);
+//	P2 = 0xEF;  //111;0 1111
+//	Delay1ms(500;
+//	P2 = 0xDF;	//1101 1111
+//	Delay1ms(500);
+//	P2 = 0xBF;	//1011 1111
+//	Delay1ms(500);
+//	P2 = 0x7F;	//0111 1111
+////	Delay1ms(500);
+//#include <REGX52.H>
+//	void Delay(unsigned int xms)		//@12.000MHz
+//	{
+//		unsigned char i, j;
+//		while (xms)
+//		{
+//			i = 12;
+//			j = 169;
+//			do
+//			{
+//			/*	while (--j);
+	//		} while (--i);
+	//		xms--;
+	//	}
+	//}
+
+	//void main();
+	//{
+	//	while (1)
+	//	{
+	//		if (P3_0 == 0)
+	//		{
+	//			Delay(200)
+	//				while (P3_0 == 0)
+	//				{
+	//					P2_0 = ~P2_0;
+	//				}
+	//			Delay(200);
+	//		}*/
+	//	}
+	//}
